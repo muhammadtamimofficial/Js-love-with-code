07a1b8c6af35b0eae9dd359ad6433d24af243b06
muhammad-tamim <mdt40234@gmail.com>
2021-07-02 12:26:12 -0700

    my git

---
+++ app.js
@@ -0,0 +1,40 @@
+// let value;
+// let num1 = 12;
+// let num2 = 5;
+
+// //Simpole Math
+// value = num1 + num2;
+// value = num1 - num2;
+// value = num1 * num2;
+// value = num1 / num2;
+// value = num1 % num2;
+
+
+// value = Math.PI ;
+// value = Math.E ;
+// value = Math.round(3.5);
+// value = Math.ceil(3.5);
+// value = Math.floor(3.5);
+// value = Math.sqrt(52);
+// value = Math.pow(8,2);
+// value = Math.abs(-5);
+// value = Math.min(2,4,2,1,54,44,34);
+// value = Math.max(2,4,2,1,54,44,34);
+// value = Math.random() *100;
+// value = Math.floor(value) +1;
+
+
+//Output
+//console.log(value);
+
+
+let firstName = prompt ("What is your name ?");
+let prtnerName = prompt ("What is your croush name ?");
+
+//console.log(firstName);
+//console.log(prtnerName);
+
+let number =Math.random()*100;
+number = Math.floor(number)+1;
+
+alert(firstName + " and " +prtnerName+" your love chance is "+number+" %");
